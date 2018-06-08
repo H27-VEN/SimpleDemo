128f3dc04f6514266a53ba494b0cbf399df23385
Hardik Lakdawala <hardik@socialpilot.co>
2018-06-08 09:30:35 +0530

    Added the php script to integerate it with react

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+?>
+
+<!DOCTYPE>
+<html>
+    <head>
+        <title>This is a demo php file</title>
+        <link rel="stylesheet" href="static/css/main.c17080f1.css">
+    </head>
+    <body>
+        <?php
+
+            echo "<div><h1>Hello I am a simple Div created This is a simple div created inside the php file</h1></div>";
+            echo "<div id='root'>";
+        ?>
+         <!-- <link rel="stylesheet" href="static/css/main.c17080f1.css"> -->
+        <script type="text/javascript" src="static/js/main.61911c33.js"></script>
+
+    </body>
+
+</html>
+
